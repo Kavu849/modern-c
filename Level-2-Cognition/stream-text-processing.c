28f6b34080a880a626ead1c3e6d6c4e8414637d8
Szymon S <sobieszekszymon@gmail.com>
2025-07-05 03:02:51 -0400

    Challenge 16: Fixed typos and improved EOF handling.

--- Level-2-Cognition/stream-text-processing.c
+++ Level-2-Cognition/stream-text-processing.c
@@ -13,6 +13,9 @@ char* fgetline(FILE* stream) {
 
   for (;;) {
     if (!fgets(line + len, size - len, stream)) {
+      if (len > 0) // If we read some characters so far, return them
+        return line;
+
       free(line);
       return nullptr;
     }
@@ -28,21 +31,26 @@ char* fgetline(FILE* stream) {
     char* tmp = realloc(line, size);
     if (!tmp) {
       free(line);
+      fprintf(stderr, "\nError while reallocating the buffer, exiting!");
       return nullptr;
     }
     line = tmp;
   }
 }
 
+// Process the string 'line' by counting every occurence of 'word' and
+// replacing it by 'replacement'. Dump the modified string to 'stream'.
 void replaceWordsAndPrint(const char* line, const char* word,
                           const char* replacement, size_t* count,
                           FILE* stream) {
   if (!line || !word)
     return;
 
-  for (const char* c = line; c; ++c) {
-    if ((c != line) && (isalnum((unsigned char)*(c-1))))
+  for (const char* c = line; *c; ++c) {
+    if ((c != line) && (isalnum((unsigned char)*(c-1)))) {
+      fprintf(stream, "%c", *c);
       continue;
+    }
 
     const char* cpos = c;
     const char* wpos = word;
@@ -53,11 +61,11 @@ void replaceWordsAndPrint(const char* line, const char* word,
     }
 
     if (!*wpos && (isspace((unsigned char)*cpos) || ispunct((unsigned char)*cpos) || *cpos == '\0')) {
-      fprintf(stdout, "%s", replacement);
+      fprintf(stream, "%s", replacement);
       ++(*count);
-      c = cpos;
+      c = cpos - 1;
     } else {
-      fprintf(stdout, "%c", *c);
+      fprintf(stream, "%c", *c);
     }
   }
 }
@@ -75,7 +83,7 @@ int main(int argc, char* argv[argc+1]) {
   char* word = argv[1];
   char* replacement = argv[2];
 
-  printf("Received arguments: \"%s\", and \"%s\".\n", word, replacement);
+  fprintf(stderr, "Received arguments: \"%s\", and \"%s\".\n", word, replacement);
  
   size_t count = 0;
 
@@ -84,15 +92,13 @@ int main(int argc, char* argv[argc+1]) {
     if (line) {
       // Process line
       replaceWordsAndPrint(line, word, replacement, &count, stdout);
+      fprintf(stdout, "\n");
       free(line);
     } else {
-      // Error
-      fprintf(stderr, "\nError while processing the line, exiting!");
-      return EXIT_FAILURE;
+      // Check if we are at the end of stream
+      fprintf(stderr, "\nFinished processing the line. Found %zu occurrences"
+              " of the word \"%s\".\n", count, word);
+      return EXIT_SUCCESS;
     }
   }
-  fprintf(stdout, "\nFound %ld occurrences of the word \"%s\", and replaced it "
-          "with the word \"%s\".\n", count, word, replacement);
-
-  return EXIT_SUCCESS;
 }

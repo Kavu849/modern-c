7537b1bc4cd02e8a9b830b873866b7e8a62a7d67
Szymon S <sobieszekszymon@gmail.com>
2025-07-18 11:04:08 -0400

    Added test cases

--- Level-1-Acquaintance/Exs47.c
+++ Level-1-Acquaintance/Exs47.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-signed safe_strcmp(char const s0[static 1], size_t n0, char const s1[static 1],
+signed safeStrcmp(char const s0[static 1], size_t n0, char const s1[static 1],
                    size_t n1) {
   // Check if s0 is null-terminated
   void* found0 = memchr(s0, '\0', n0);
@@ -23,5 +23,22 @@ signed safe_strcmp(char const s0[static 1], size_t n0, char const s1[static 1],
 }
 
 int main(int argc, char* argv[argc+1]) {
+  // Test the functionality of safeStrcmp
+  signed res1 = safeStrcmp("apple", 6, "apple", 6);
+  printf("Comparison of \"apple\" and \"apple\" results in %d\n", res1);
+  signed res2 = safeStrcmp("apples", 7, "apple", 6);
+  printf("Comparison of \"apples\" and \"apple\" results in %d\n", res2);
+  signed res3 = safeStrcmp("zebra", 6, "yak", 4);
+  printf("Comparison of \"zebra\" and \"yak\" results in %d\n", res3);
+  signed res4 = safeStrcmp("apple", 6, "apply", 6);
+  printf("Comparison of \"apple\" and \"apply\" results in %d\n", res4);
+
+  char bufferNoNull[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+  char bufferShort[5] = "abc";
+
+  signed res5 = safeStrcmp(bufferNoNull, 8, bufferShort, 4);
+  printf("Comparison of non null-terminated bufferNoNull and null-terminated"
+         " bufferShort results in %d\n", res5);
+  
   return 0;
 }

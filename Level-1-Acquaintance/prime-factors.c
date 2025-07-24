fb842964640625abc4603c6fb3d35c325c917112
Szymon S <sobieszekszymon@gmail.com>
2025-07-24 10:27:59 -0400

    Reduce the number of loop iterations by exiting earlier if the rolling number is less than the square root of 'n'. If we don't find any factor in the loop, 'n' must be prime

--- Level-1-Acquaintance/prime-factors.c
+++ Level-1-Acquaintance/prime-factors.c
@@ -5,15 +5,25 @@ size_t smallestPrimeFactor(size_t n) {
   if (n % 2 == 0)
     return 2;
   
-  for (size_t i = 3; i <= n; i += 2) {
+  for (size_t i = 3; i * i <= n; i += 2) {
     if (n % i == 0)
       return i;
   }
-  return 0;
+  // Otherwise, n itself must be prime
+  return n;
 }
 
 void findPrimeFactors(size_t n) {
   printf("Prime factors of %zu:", n);
+  if (n == 0) {
+    printf(" undefined, cannot factor 0\n");
+    return;
+  }
+  if (n == 1) {
+    printf(" 1\n");
+    return;
+  }
+
   while (n != 1) {
     size_t factor = smallestPrimeFactor(n);
     printf(" %zu", factor);

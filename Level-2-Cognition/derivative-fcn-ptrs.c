d13a42c4da304336178c60a6e1d186a07b27b4d4
SzymonS <sobieszekszymon@gmail.com>
2025-08-25 18:00:54 -0400

    Added implementations of polynomial evaluation, and polynomial deflation using Ruffini's rule.

--- Level-2-Cognition/derivative-fcn-ptrs.c
+++ Level-2-Cognition/derivative-fcn-ptrs.c
@@ -2,6 +2,13 @@
 #include <float.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Polynomial Polynomial;
+struct Polynomial {
+  size_t deg; // degree of the polynomial
+  double* coeff; // array of coefficients, must be of size deg + 1
+};
 
 double centeredDiffReal(double (*f)(double), double x0) {
   if (!f)
@@ -54,14 +61,56 @@ double findZero(double (*f)(double), double x0) {
   return x1;
 }
 
+// Evaluate polynomial p at x
+double evalPoly(Polynomial* p, double x) {
+  double val = p->coeff[0];
+  size_t degree = p->deg;
+  double x0 = x;
+
+  for (size_t i = 1; i <= degree; ++i) {
+    val += p->coeff[i] * x0;
+    x0 *= x;
+  }
+
+  return val;
+}
+
+// Divide polynomial p by (x - root), and return the resulting polynomial
+Polynomial* deflatePoly(Polynomial*p, double root) {
+  Polynomial* q = malloc(sizeof(Polynomial));
+  printf("1\n");
+  size_t NewDeg = p->deg - 1;
+  q->deg = NewDeg;
+  q->coeff = malloc((NewDeg + 1) * sizeof(double));
+  q->coeff[NewDeg] = p->coeff[p->deg];
+  
+  for (size_t i = p->deg - 1; i > 0; --i)
+    q->coeff[i - 1] = q->coeff[i] * root + p->coeff[i];   
+
+  return q;
+}
+
 int main(int argc, char* argv[argc + 1]) {
-  complex (*g)(complex) = csin;
-  centeredDiffComplex(g, CMPLX(0.0, 1.0));
+  //complex (*g)(complex) = csin;
+  //centeredDiffComplex(g, CMPLX(0.0, 1.0));
 
   double (*f)(double) = cos;
   double x0 = 1.0;
   double root = findZero(f, x0);
   printf("Found root of cosine: %f\n", root);
 
+  Polynomial p;
+  p.deg = 3;
+  p.coeff = malloc(p.deg * sizeof(double));
+  p.coeff[0] = -4.0;
+  p.coeff[1] = 0.0;
+  p.coeff[2] = 3.0;
+  p.coeff[3] = 2.0;
+  //double val = evalPoly(&p, 2.0);
+  Polynomial* q = deflatePoly(&p, -1.0);
+  
+  free(p.coeff);
+  free(q->coeff);
+  free(q);
   return 0;
 }

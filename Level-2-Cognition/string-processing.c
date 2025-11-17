35b633f339e83dc9cebbaf0640c3c0a0328014e2
Szymon S <sobieszekszymon@gmail.com>
2025-11-17 11:51:57 -0500

    Challenge 15: improved definition of finding a word in a string, and added a function replacing a word in a string with a different word. Also, replaced all instances of "nullptr" with "NULL".

--- Level-2-Cognition/string-processing.c
+++ Level-2-Cognition/string-processing.c
@@ -1,15 +1,22 @@
 #include "string-processing.h"
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-const char* findWord(const char* word, const char* s) {
+const char* findWord(const char* s, const char* word) {
   if (!word || !s)
-    return nullptr;
+    return NULL;
 
-  const char* found = nullptr;
+  const char* found = NULL;
+  const char* beg_str = s;
 
   for (const char* c = s; *c; ++c) {
+    // Make sure to start at the beginning of a word
+    if ((c != beg_str) && (!isspace(*(c-1))))
+      continue;
+
     const char* cpos = c;
     const char* wpos = word;
 
@@ -18,25 +25,77 @@ const char* findWord(const char* word, const char* s) {
       ++wpos;
     }
 
-    if (!*wpos) {
+    if (!*wpos && (isspace(*cpos) || ispunct(*cpos) || *cpos == '\0')) {
       found = c;
       break; // return the beginning of 'word' in 's'
     }
   }
 
-  //if (found == s) 
   if (!found)
-    return nullptr;
+    return NULL;
   
   return found;
+}
+
+char* replaceWord(const char* s, const char* word, const char* replacement) {
+  if (!word || !s || !replacement)
+    return NULL;
+
+  const char* found = findWord(s, word);
+
+  if (!found)
+    return strdup(s);
+
+  // Create a new string, which we use to compose the string after replacement
+  char* newS = malloc(strlen(s) - strlen(word) + strlen(replacement) + 1);
+  char* newSBeg = newS;
+
+  while (s != found) {
+    *newS = *s;
+    ++s;
+    ++newS;
+  }
+
+  while (*word) {
+    ++s;
+    ++word;
+  }
+
+  while (*replacement) {
+    *newS = *replacement;
+    ++replacement;
+    ++newS;
+  }
+
+  while (*s) {
+    *newS = *s;
+    ++s;
+    ++newS;
+  }
+
+  *newS = '\0';
+  return newSBeg;
 }
 
 int main(int argc, char* argv[argc+1]) {
   char str[] = "This is a string for testing purposes\n";
   printf("%s", str);
   const char word[] = "is";
-  const char* find = findWord(word, str);
-  if (find)
-    printf("%c\n", *find);
+  const char* found = findWord(str, word);
+  if (found) {
+    printf("Found the word: \"%s\", as \"", word);
+    const char* it = word;
+    while (*it) {
+      printf("%c", *it);
+      ++it;
+    }
+    printf("\"\n");
+
+    char replacement[] = "verifying";
+    printf("Replacing \"%s\" with \"%s\"\n", word, replacement);
+    char* newS = replaceWord(str, word, replacement);
+    printf("String after replacement: %s \n", newS);
+  } else
+    printf("Didn't find the word: %s \n", word);
   return EXIT_SUCCESS;
 }

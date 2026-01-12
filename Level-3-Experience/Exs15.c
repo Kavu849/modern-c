aee7d79369268c041f4edbb508e8b4cd527b191b
SzymonS <sobieszekszymon@gmail.com>
2026-01-11 21:54:59 -0500

    Exercise 15: Replacing a variadic function with a variadic macro and an inline function that takes an array.

---
+++ Level-3-Experience/Exs15.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stddef.h>
+
+double sumIt(size_t n, double* a);
+
+#define SUM_DOUBLE(...) sumIt(sizeof((double[]){ __VA_ARGS__ }) / sizeof(double), (double[]) { __VA_ARGS__ })
+
+inline double sumIt(size_t n, double* a) {
+  double ret = 0.0;
+  for (size_t i = 0; i < n; ++i) {
+    ret += a[i];
+  }
+  return ret;
+}
+
+int main(void) {
+  double ret = SUM_DOUBLE( 1.0, 2.0, 1.0, -1.0, 0.0 );
+  printf("Sum: %f\n", ret);
+  return 0;
+}

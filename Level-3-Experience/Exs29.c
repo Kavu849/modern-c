d5fedbe80ead564dd206612d39b255fa84447cc5
Szymon S <sobieszekszymon@gmail.com>
2026-01-30 10:47:36 -0500

    Exercise 29: modified the macro so that it accepts an array or pointer A with base b and tests whether B is compatible to b*. The trick here is to add 0 to A in the controlling expression to force decay of A to a pointer if it's an array, and no change if it's a pointer.

---
+++ Level-3-Experience/Exs29.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define static_assert_compatible(A, B, REASON)                  \
+  static_assert(_Generic((typeof(A+0)*)nullptr,                 \
+                         typeof(B)*: true,                      \
+                         default: false),                       \
+                "expected compatible types: " REASON ", have " #A " and " #B "")
+
+#define SWAP(X, Y)                                              \
+  do {                                                          \
+    /* These two variables have the role of function            \
+       parameters. They ensure to evaluate the                  \
+       expression X and Y once, since these could be            \
+       complicated lvalue expressions with valuated             \
+       subexpressions that have side effects. */                \
+    auto const swap_p1 = &(X);                                  \
+    auto const swap_p2 = &(Y);                                  \
+    static_assert_compatible(*swap_p1, *swap_p2,                \
+                             "to exchange values, '"            \
+                             #X "' and '" #Y                    \
+                             "' must have compatible types");   \
+    auto swap_tmp = *swap_p1;                                   \
+    *swap_p1 = *swap_p2;                                        \
+    *swap_p2 = swap_tmp;                                        \
+  } while (false)
+
+int main() {
+  int x = 1;
+  int y = 2;
+  printf("Before the swap: x = %d, y = %d\n", x, y);
+  SWAP(x, y);
+  printf("After the swap: x = %d, y = %d\n", x, y);
+  
+  long z = 3;
+  SWAP(x, z); // This should fail the static assert with the correct message
+
+  return 0;
+}
